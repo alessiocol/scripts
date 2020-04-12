e071bdbe1184a2e99ae11340443a1b5067b8d426
Alessio Colombo <alessio.colombo@tomtom.com>
2020-04-12 22:59:15 +0200

    add update_ip script

--- backup-home/backup-me.sh
+++ backup-home/backup-me.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/alessio/ /import/home/alessio/ubuntu-home-backup
+rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/alessio/ /media/alessio/remote-home/backup/ubuntu-home
---
+++ update_ip_godaddy/README.md
@@ -0,0 +1,4 @@
+# Update DNS record on GoDaddy.com to the current IP
+Sets the DNS record to current public IP address using GoDaddy.com developers API. 
+
+The script needs to be customized!
