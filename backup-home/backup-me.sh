395cee5c929cfc5d7a43d886bb87c91f5906a80f
Alessio Colombo <alessio.colombo@tomtom.com>
2020-05-01 14:09:56 +0200

    excluded further directories from backup

--- backup-home/backup-me.sh
+++ backup-home/backup-me.sh
@@ -1,3 +1,11 @@
 #! /usr/bin/env bash
 
-rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/alessio/ /media/alessio/remote-home/backup/ubuntu-home
+USER=$(whoami)
+
+# mount cmd
+#  sudo mount -t cifs -o username=${USER},uid=${USER},gid=${USER},vers=1.0 //192.168.178.3/home /media/${USER}/remote-home
+
+REMOTE="/media/${USER}/remote-home/backup/ubuntu-home"
+#REMOTE="/var/run/user/${UID}/gvfs/smb-share:server=192.168.178.3,share=home/backup/ubuntu-home"
+
+rsync -aP --exclude-from=rsync-homedir-excludes.txt /home/${USER}/ "${REMOTE}"
